f003a64e33655f384f9135ecf13075521f5bcee1
Mayurs0 <mayurs@iconnectsolutions.com>
2024-02-28 07:40:05 +0530

    hbs

--- todoapp/routes/users.js
+++ todoapp/routes/users.js
@@ -40,7 +40,7 @@ router.get("/", function (req, res, next) {
 router.get("/list", function (req, res, next) {
   res.render("users", { data: data });
 });
-router.get("/userprofile", function (req, res, next) {
+router.get("/userprofile/:id", function (req, res, next) {
   res.render("userprofile"); //express automaticlly go and find file and render it
 });
 
