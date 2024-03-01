dc726997f99fee35890156f09504e686c3157b63
Mayurs0 <mayurs@iconnectsolutions.com>
2024-03-01 09:58:30 +0530

    Middleware resolved

--- middleware/main.js
+++ middleware/main.js
@@ -9,6 +9,10 @@ app.use(express.json(), Uppercase, appLevel, appLevel2);
 const server = () => {
   //route level middleware
   app.post("/data", first, second, (req, res, next) => {
+    //req.send("")   // if you send any info must be in double quote not in single bcz it consider it as char
+    // res.send("data",req.body);
+    // You can only send one response and if you want to send two then the second one is status code
+    // res.send(req.body, 200);
     res.send(req.body);
   });
   app.listen("8090", () => {
